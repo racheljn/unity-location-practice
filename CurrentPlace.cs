47f1b5b6df3ba6db9e28661f089f4bcd9f2e9c60
Rachel Nguyen <35277830+racheljn@users.noreply.github.com>
2019-11-09 22:21:48 +0900

    tried to add other locations, but broken

--- CurrentPlace.cs
+++ CurrentPlace.cs
@@ -21,6 +21,26 @@ public class CurrentPlace : MonoBehaviour
     {
         // Checks if latitude and longitude is in range
         // Note: Put smallest lat/long first, then greater lat/long
+        // GS25, Gonzaga Hall, Cafeteria
+        if  (Lat >= 37.5508 - MarginOfError && Lat <= 37.5510 + MarginOfError && Long >= 126.9436 - MarginOfError && Long <= 126.94440 + MarginOfError
+                || Lat >= 37.5505 - MarginOfError && Lat <= 37.5513 + MarginOfError && Long >= 126.9435 - MarginOfError && Long <= 126.9437 + MarginOfError
+                || Lat >= 37.5502 - MarginOfError && Lat <= 37.5509 + MarginOfError && Long >= 126.9387 - MarginOfError && Long <= 126.9391)       
+        {
+            return "Eating Area";
+        }        
+        else if (Lat >= 37.5508 - MarginOfError && Lat <= 37.5517 - MarginOfError && Long >= 126.9439 - MarginOfError && Long <= 126.9440 + MarginOfError) {
+            return "Gonzaga Hall";
+        }
+        else if (Lat >= 37.5513 - MarginOfError && Lat <= 37.5517 && Long <= 126.9428 - MarginOfError && Long >= 126.9437 + MarginOfError)
+        {
+            return "Classroom";
+        }
+        else if (Lat >= 37.5503 - MarginOfError && Lat <= 37.5507 + MarginOfError && Long <= 126.9406 - MarginOfError && Long >= 126.9421 + MarginOfError)
+        {
+            return "Field";
+        }
+
+
         if (Lat > 37.54906 - MarginOfError && Lat < 37.54906 + MarginOfError && Long > 126.9386 - MarginOfError && Long < 126.9386 + MarginOfError)
         {
             return "Cafe";
